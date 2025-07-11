2431fe84e5f1ba7f2e488dd713217afd4b44846e
Michaela Poštulka <michaelamosova@gmail.com>
2025-07-11 13:32:58 +0200

    feat: added button for admin section into user ctx menu

--- src/components/auth/user-button.tsx
+++ src/components/auth/user-button.tsx
@@ -42,6 +42,9 @@ export default async function UserButton() {
           <Link href="/profile" className="w-full">
             <Button className="w-full">Profile</Button>
           </Link>
+          <Link href="/profile" className="w-full">
+            <Button className="w-full">Administration</Button>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
           <SignOut className="w-full" />
