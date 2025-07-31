542dbe7a8fd5ae193e947b6bce666590fee633dc
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-07-31 16:23:43 +0200

    CZBANK-53: Change logo to new one and add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <ThemeProvider attribute="class" defaultTheme="system">
             <div className="flex flex-row items-center justify-between">
               <Link href={"/"} className="flex flex-row items-center space-x-2">
-                <Image src={`/logo.png`} alt={"Logo"} width="30" height="30" />
+                <Image src={`/logo.svg`} alt={"Logo"} width="50" height="50" />
                 <span className="text-2xl font-bold">CzechiBank</span>
               </Link>
               <div className="flex flex-1 items-center justify-end space-x-2 p-2">
