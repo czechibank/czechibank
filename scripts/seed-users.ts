999744b3755a6588d67dcd8bcd0b46d3efbbd27d
Silvia Strazovcova <silviczka@gmail.com>
2025-10-01 19:27:14 +0200

    chore: added Silvia as Dev in contributors

--- scripts/seed-users.ts
+++ scripts/seed-users.ts
@@ -80,6 +80,17 @@ const usersToSeed = [
     apiKey: "66",
     role: "admin",
   },
+
+  {
+    email: "silviczka@czechibank.ostrava.digital",
+    name: "[CZBANK][💛 DEV] Silvi Czka",
+    password: "hello123456",
+    avatarConfig:
+      '{"backgroundColor":["C4DD68"],"eyebrows":["variant12"],"eyebrowsColor":["000000"],"eyes":["variant01"],"eyesColor":["000000"],"freckles":["variant01"],"frecklesColor":["000000"],"frecklesProbability":[null],"glasses":["variant03"],"glassesColor":["000000"],"glassesProbability":[null],"mouth":["happy05"],"mouthColor":["000000"],"nose":["variant06"],"noseColor":["000000"]}',
+    bankAccountNumber: "000000000005/5555",
+    apiKey: "77",
+    role: "user",
+  },
 ];
 
 async function seedUsers() {
