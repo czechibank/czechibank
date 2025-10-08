07663a14914f6a22636484d34fce97e7ce08d21e
Vojtech Cerveny <cervik49@gmail.com>
2025-10-09 00:05:52 +0200

    fix: increase rateLimit

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const MIN_PASSWORD_LENGTH = 8;
 
 export const RATE_LIMIT = {
   TIME_WINDOW: 1000 * 60 * 1, // 1 minute
-  MAX_REQUESTS: 20, // 20 requests per minute
+  MAX_REQUESTS: 60, // 60 requests per minute
 };
 
 export const SESSION = {
