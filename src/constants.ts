0db7792ae036266f6d42a808fc0728a43817a10d
Vojtěch Červený <12935892+vojtech-cerveny@users.noreply.github.com>
2026-02-17 11:35:13 +0100

    fix: increase rateLimit (#32)

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,11 @@
 export const MIN_PASSWORD_LENGTH = 8;
+
+export const RATE_LIMIT = {
+  TIME_WINDOW: 1000 * 60 * 1, // 1 minute
+  MAX_REQUESTS: 60, // 60 requests per minute
+};
+
+export const SESSION = {
+  EXPIRES_IN: 60 * 30, // 30 minutes
+  UPDATE_AGE: 60 * 5, // 5 minutes (every 5 minutes the session expiration is updated)
+};
