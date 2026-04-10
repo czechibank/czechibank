304e0ea45084b95d9808d40d296c594617f60917
Michaela Poštulka <michaelamosova@gmail.com>
2026-04-10 18:26:52 +0200

    fix: responsive bank account cards on mobile

--- src/components/bank-account/ba-list.tsx
+++ src/components/bank-account/ba-list.tsx
@@ -53,7 +53,7 @@ export default function BankAccountsList({ initialBankAccounts, session }: BankA
           onCreated={(newBankAccount: BankAccount) => setAccounts((prev) => [...prev, newBankAccount])}
         />
       </div>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         {accounts.map((ba) => (
           //comment Silviczka> div added to avoid oppening of bank account card while renaming
           <div key={ba.id}>
